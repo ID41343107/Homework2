2aff89b6542b8feafcf1c9337834479a683198fe
yucheng-wu <41343109@nfu.edu.tw>
2026-05-03 20:15:32 +0800

    dfs bfs

--- src/DFS and BFS main.cpp
+++ src/DFS and BFS main.cpp
@@ -1,148 +1,77 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <climits>
+#include <tuple>
 #include <algorithm>
+
 using namespace std;
 
 class Graph {
+public:
+    virtual ~Graph() {}
+
+    virtual void InsertEdge(int u, int v, int w = 1) = 0;
+
+protected:
+    int n = 0;
+    int e = 0;
+};
+class TraversalGraph : public Graph {
 private:
-    int n;
     vector<vector<int>> adj;
-    vector<bool> visited;
 
 public:
-    Graph(int vertices);
-    void addEdge(int u, int v);
-
-    void DFS();
-    void DFS(int v);
-
-    void BFS(int v);
-};
-
-Graph::Graph(int vertices)
-{
-    n = vertices;
-    adj.resize(n);
-    visited.resize(n, false);
-}
-
-void Graph::addEdge(int u, int v)
-{
-    if (u >= n || v >= n) return;
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-}
-
-// ===== DFS =====
-void Graph::DFS()
-{
-    fill(visited.begin(), visited.end(), false);
-
-    cout << "DFS traversal: ";
-
-    for (int i = 0; i < n; i++) // 處理非連通圖
-    {
-        if (!visited[i])
-            DFS(i);
+    TraversalGraph(int n) {
+        this->n = n;
+        adj.resize(n);
     }
 
-    cout << endl;
-}
-
-void Graph::DFS(int v)
-{
-    visited[v] = true;
-    cout << v << " ";
-
-    for (int w : adj[v])
-    {
-        if (!visited[w])
-            DFS(w);
+    void InsertEdge(int u, int v, int w = 1) override {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-}
 
-// ===== BFS =====
-void Graph::BFS(int start)
-{
-    fill(visited.begin(), visited.end(), false);
+    // DFS
+    void DFSUtil(int u, vector<bool>& visited) {
+        visited[u] = true;
+        cout << u << " ";
 
-    queue<int> q;
-
-    cout << "BFS traversal: ";
-
-    visited[start] = true;
-    q.push(start);
-
-    while (!q.empty())
-    {
-        int v = q.front();
-        q.pop();
-
-        cout << v << " ";
-
-        for (int w : adj[v])
-        {
-            if (!visited[w])
-            {
-                visited[w] = true;
-                q.push(w);
-            }
+        for (int v : adj[u]) {
+            if (!visited[v]) DFSUtil(v, visited);
         }
     }
 
-    // 處理非連通圖
-    for (int i = 0; i < n; i++)
-    {
-        if (!visited[i])
-        {
-            visited[i] = true;
-            q.push(i);
+    void DFS(int start) {
+        vector<bool> visited(n, false);
+        cout << "DFS: ";
+        DFSUtil(start, visited);
+        cout << endl;
+    }
 
-            while (!q.empty())
-            {
-                int v = q.front();
-                q.pop();
+    // BFS
+    void BFS(int start) {
+        vector<bool> visited(n, false);
+        queue<int> q;
 
-                cout << v << " ";
+        visited[start] = true;
+        q.push(start);
 
-                for (int w : adj[v])
-                {
-                    if (!visited[w])
-                    {
-                        visited[w] = true;
-                        q.push(w);
-                    }
+        cout << "BFS: ";
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            cout << u << " ";
+
+            for (int v : adj[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.push(v);
                 }
             }
         }
+        cout << endl;
     }
-
-    cout << endl;
-}
-
-//--------------------------------------
-/*
-        0
-      /   \
-     1     2
-    / \     \
-   3   4     5
-*/
-//--------------------------------------
-
-int main()
-{
-    Graph g(6);
-
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 5);
-
-    g.DFS();
-    g.BFS(0);
-
-    return 0;
-}
+};
